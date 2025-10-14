c484ce33f70d5e22e6276dba339cba88afc2727f
texidot <alexandre.grosdidier@edu.esiee.fr>
2025-10-14 09:23:16 +0200

    updated td5 ex 4 - created variables for intAverage and floatAverage

--- td5/src/ex4.cpp
+++ td5/src/ex4.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int average(const vector<int>& arr, const int& size) {
+int average(const vector<int>& arr, const int& size) { // en int
     if (size <= 0) return 0;
     int sum {0};
     for (int i = 0; i < size; ++i) {
@@ -11,7 +11,7 @@ int average(const vector<int>& arr, const int& size) {
     return sum / size;
 }
 
-float average(const vector<float>& arr, const int& size) {
+float average(const vector<float>& arr, const int& size) { // en float
     if (size <= 0) return 0.0f;
     float sum {0.0f};
     for (int i = 0; i < size; ++i) {
@@ -24,8 +24,11 @@ int main() {
     vector<int> intArray = {1, 2, 3, 4, 5};
     vector<float> floatArray = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f};
 
-    cout << "La moyenne des entiers est " << average(intArray, intArray.size()) << endl;
-    cout << "La moyenne des flottants est " << average(floatArray, floatArray.size()) << endl;
+    int intAverage = average(intArray, intArray.size());
+    float floatAverage = average(floatArray, floatArray.size());
+
+    cout << "La moyenne des entiers est " << intAverage << endl;
+    cout << "La moyenne des flottants est " << floatAverage << endl;
 
     return 0;
 }
